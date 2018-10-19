c447b58f147b19122aafafc6a1e490722260b786
Mogria <m0gr14@gmail.com>
2018-10-19 09:58:20 +0200

    add default.nix with virtualenv and load it both with direnv

---
+++ default.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> { }, stdenv ? pkgs.stdenv }:
+
+
+let
+  pythonPackages = pkgs.python36Packages;
+  opencv3 = ( pkgs.opencv3.override {
+    enableGtk3 = true;
+    enablePython = true;
+    inherit pythonPackages;
+  } );
+in stdenv.mkDerivation rec {
+  name = "mollyvision-opencv-python-env-${version}";
+  version = "0.0.1";
+  propagatedBuildInputs = [
+    ( pythonPackages.python.withPackages
+      (ps: [ ps.virtualenv (ps.toPythonModule opencv3) ] ) )
+    opencv3
+  ];
+}
