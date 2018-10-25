388da112a12d343825b1559e90d01fe0ce96f809
Remo Schwarzentruber <Remo.Schwarzentruber@stud.hslu.ch>
2018-10-25 08:57:10 +0200

    protobuf => version 3.5

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@
 let
   pythonPackages = pkgs.python36Packages;
   opencv3 = ( pkgs.opencv3.override {
+    protobuf = pkgs.protobuf3_5;
     enableGtk3 = true;
     enablePython = true;
     inherit pythonPackages;
