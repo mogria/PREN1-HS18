484927af87cf2abad92eb95fa817577aa705985b
Mogria <m0gr14@gmail.com>
2018-11-01 20:51:14 +0100

    remove debug code from imageutil

--- mollyvision/imageutil.py
+++ mollyvision/imageutil.py
@@ -12,7 +12,6 @@ def read_image_folder(folder, recurse=False):
     globs = [ os.path.join(glob.escape(folder), pattern + "." + extension) for extension in extensions ]
 
     matches_list = [ glob.glob(glob_pattern, recursive=recurse) for glob_pattern in globs ]
-    print(*matches_list)
 
     # flatten 2d list into 1d, and make it unique
     return set(itertools.chain(*matches_list))
